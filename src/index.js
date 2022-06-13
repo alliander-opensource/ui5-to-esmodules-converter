c3ef63bc89312737b95d8f11933a1c4668056313
Jordy van Londen <jordy.vanlonden@gmail.com>
2022-06-13 17:56:49 +0200

    Linting enabled

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-const {readFileSync, writeFileSync} = require('fs');
-const {generate, parse, replace, traverse} = require('abstract-syntax-tree');
+const { readFileSync, writeFileSync } = require("fs");
+const { generate, parse, replace, traverse } = require("abstract-syntax-tree");
 const _ = require("lodash");
+const {
+  isFunctionExpression,
+  isObjectExpression,
+  isSapUiRequire,
+  isUi5Extend,
+  isReturnStatement,
+} = require("./matchers");
 
 // import {program} from 'commander';
 //
@@ -8,140 +15,112 @@ const _ = require("lodash");
 // const command = program.parse();
 // command.
 
-const isSapUiRequire = _.matches({
-    type: 'ExpressionStatement',
-    expression: {
-        type: 'CallExpression',
-        callee: {
-            type: 'MemberExpression',
-            object: {
-                type: 'MemberExpression',
-                object: {type: 'Identifier', name: 'sap'},
-                property: {type: 'Identifier', name: 'ui'},
-            },
-            property: {type: 'Identifier', name: 'define'},
-        }
-    }
-});
-
-const isReturnStatement = _.matches({type: 'ReturnStatement'});
-
-const isFunctionExpression = _.matches({type: 'FunctionExpression'})
-
-const isObjectExpression = _.matches({type: 'ObjectExpression'})
-
-const isUi5Extend = _.matches({
-    type: "CallExpression",
-    callee: {
-        type: "MemberExpression",
-        property: {type: "Identifier", name: "extend"}
-    }
-})
-
 function convertDependencyToImport(dependency) {
-    return {
-        type: 'ImportDeclaration',
-        source: {
-            type: 'Literal',
-            value: dependency.value
+  return {
+    type: "ImportDeclaration",
+    source: {
+      type: "Literal",
+      value: dependency.value,
+    },
+    specifiers: [
+      {
+        type: "ImportDefaultSpecifier",
+        local: {
+          type: "Identifier",
+          name: _.last(dependency.value.split("/")),
         },
-        specifiers: [{
-            type: 'ImportDefaultSpecifier',
-            local: {
-                type: 'Identifier',
-                name: _.last(dependency.value.split('/')),
-            }
-        }]
-    };
-
+      },
+    ],
+  };
 }
-function createDefaultExport(declaration) {
-    return {
-        type: 'ExportDefaultDeclaration',
-        declaration,
-    };
 
+function createDefaultExport(declaration) {
+  return {
+    type: "ExportDefaultDeclaration",
+    declaration,
+  };
 }
 
 function convertPropertyToClassMember(property) {
-    if (isFunctionExpression(property.value)) {
-        return {
-            type: 'MethodDefinition',
-            kind: 'method',
-            static: false,
-            key: property.key,
-            value: property.value,
-            computed: property.computed,
-        };
-    }
+  if (isFunctionExpression(property.value)) {
+    return {
+      type: "MethodDefinition",
+      kind: "method",
+      static: false,
+      key: property.key,
+      value: property.value,
+      computed: property.computed,
+    };
+  }
 
-    if (isObjectExpression(property.value)) {
-        return {
-            type: 'PropertyDefinition',
-            key: property.key,
-            value: property.value,
-            computed: property.computed,
-            decorators: [],
-            static: true,
-        }
-    }
+  if (isObjectExpression(property.value)) {
+    return {
+      type: "PropertyDefinition",
+      key: property.key,
+      value: property.value,
+      computed: property.computed,
+      decorators: [],
+      static: true,
+    };
+  }
 
-    throw new Error('Unimplemented');
+  throw new Error("Unimplemented");
 }
 
 function convertObjectExpressionToClassBody(objectExpression) {
-    return {
-        type: 'ClassBody',
-        body: objectExpression.properties.map(convertPropertyToClassMember)
-    }
+  return {
+    type: "ClassBody",
+    body: objectExpression.properties.map(convertPropertyToClassMember),
+  };
 }
 
+function replacer(node) {
+  if (isSapUiRequire(node)) {
+    const dependencies = node.expression.arguments[0].elements;
+    const globalStatements = node.expression.arguments[1].body.body;
+    const returnStatement = globalStatements.find(isReturnStatement);
 
+    const globalNonReturnStatements = globalStatements.filter(
+        (statement) => !isReturnStatement(statement)
+    );
+    const importStatements = dependencies.map(convertDependencyToImport);
+    const exportStatement = createDefaultExport(returnStatement.argument);
 
-// console.log(JSON.stringify(tree, null, 2));
+    return [
+      ...importStatements,
+      ...globalNonReturnStatements,
+      exportStatement,
+    ];
+  }
 
-const srcPath = '/home/vanlonden/src/monteursapp/webapp/Component.js'
+  if (isUi5Extend(node)) {
+    return {
+      type: "ClassDeclaration",
+      id: {
+        type: "Identifier",
+        name: _.last(node.arguments[0].value.split(".")),
+      },
+      superClass: node.callee.object,
+      body: convertObjectExpressionToClassBody(node.arguments[1]),
+    };
+  }
 
+  return node;
+}
 
-const sourcesIn = readFileSync(srcPath, 'utf-8');
+const srcPath = "/home/vanlonden/src/monteursapp/webapp/Component.js";
+const sourcesIn = readFileSync(srcPath, "utf-8");
 const tree = parse(sourcesIn, {
-    next: true
-})
+  next: true,
+});
 
 traverse(tree, {
-    enter(node) {
-        replace(node, node => {
-            if (isSapUiRequire(node)) {
-                const dependencies = node.expression.arguments[0].elements;
-                const globalStatements = node.expression.arguments[1].body.body;
-                const returnStatement = globalStatements.find(isReturnStatement);
-
-                const globalNonReturnStatements = globalStatements.filter(statement => !isReturnStatement(statement));
-                const importStatements = dependencies.map(convertDependencyToImport)
-                const exportStatement = createDefaultExport(returnStatement.argument);
-
-                return [...importStatements, ...globalNonReturnStatements, exportStatement];
-            }
-
-            if (isUi5Extend(node)) {
-                return {
-                    type: 'ClassDeclaration',
-                    id: {type: 'Identifier', name: _.last(node.arguments[0].value.split('.'))},
-                    superClass: node.callee.object,
-                    body: convertObjectExpressionToClassBody(node.arguments[1]),
-                }
-            }
-
-            return node;
-        })
-    },
-    leave(node) {
-
-    }
+  enter(node) {
+    replace(node, replacer);
+  },
+  leave(node) {},
 });
 
 const sourcesOut = generate(tree);
 
 writeFileSync(srcPath, Buffer.from(sourcesOut));
-
-
---
+++ src/matchers.js
@@ -0,0 +1,39 @@
+const _ = require("lodash");
+
+const isSapUiRequire = _.matches({
+  type: "ExpressionStatement",
+  expression: {
+    type: "CallExpression",
+    callee: {
+      type: "MemberExpression",
+      object: {
+        type: "MemberExpression",
+        object: { type: "Identifier", name: "sap" },
+        property: { type: "Identifier", name: "ui" },
+      },
+      property: { type: "Identifier", name: "define" },
+    },
+  },
+});
+
+const isReturnStatement = _.matches({ type: "ReturnStatement" });
+
+const isFunctionExpression = _.matches({ type: "FunctionExpression" });
+
+const isObjectExpression = _.matches({ type: "ObjectExpression" });
+
+const isUi5Extend = _.matches({
+  type: "CallExpression",
+  callee: {
+    type: "MemberExpression",
+    property: { type: "Identifier", name: "extend" },
+  },
+});
+
+module.exports = {
+  isSapUiRequire,
+  isReturnStatement,
+  isFunctionExpression,
+  isObjectExpression,
+  isUi5Extend,
+};
